7d384708953722ec6e93deda54e41096683fe6d1
Eric <udlis.eric@gmail.com>
2021-06-25 21:24:54 -0500

    Pre commit init (#4)
    * Added pre-commit
    
    * Add virtualenv to setup script
    
    * Ran linter
    
    * Add documentation
    
    * Add CI
    
    * keep shell in cwd

--- embedded/devices/include/mcp23017.h
+++ embedded/devices/include/mcp23017.h
@@ -1,39 +1,34 @@
 #ifndef __mcp__h__
 #define __mcp__h__
 #include <stdint.h>
+
 #include "i2c.h"
 
-#define MCP_DIRA_REG    0x00
-#define MCP_DIRB_REG    0x01
-#define MCP_GPIOA_REG   0x12
-#define MCP_GPIOB_REG   0x13
+#define MCP_DIRA_REG 0x00
+#define MCP_DIRB_REG 0x01
+#define MCP_GPIOA_REG 0x12
+#define MCP_GPIOB_REG 0x13
 
-#define MCP_NUM_PINS    16
+#define MCP_NUM_PINS 16
 
 #define IS_BANK_A(pin) (pin < (MCP_NUM_PINS >> 1))
 #define GET_REL_PIN(pin) (pin & 0x7)
 
-static const uint8_t rick_directions[16] = 
-{
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0
-};
+static const uint8_t rick_directions[16] = {0, 0, 0, 0, 0, 0, 0, 0,
+                                            0, 0, 0, 0, 0, 0, 0, 0};
 
-static const uint8_t morty_directions[16] =
-{
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0
-};
+static const uint8_t morty_directions[16] = {0, 0, 0, 0, 0, 0, 0, 0,
+                                             0, 0, 0, 0, 0, 0, 0, 0};
+class Mcp23017 : private I2c {
+ private:
+  int clear_settings();
+  int set_dir(int pin, uint8_t dir);
+  uint8_t get_dir(int pin);
 
-class Mcp23017: private I2c {
-    private:
-        int clear_settings();
-        int set_dir(int pin, uint8_t dir);
-        uint8_t get_dir(int pin);
-    public:
-        Mcp23017(int bus, int addr);
-        int begin(const uint8_t directions[]);
-        uint8_t get_state(int pin);
-        int set_state(int pin, uint8_t val);
+ public:
+  Mcp23017(int bus, int addr);
+  int begin(const uint8_t directions[]);
+  uint8_t get_state(int pin);
+  int set_state(int pin, uint8_t val);
 };
 #endif
--- embedded/devices/src/mcp23017.cpp
+++ embedded/devices/src/mcp23017.cpp
@@ -1,127 +1,122 @@
-#include <iostream>
-#include <stdint.h>
+#include "mcp23017.h"
+
 #include <errno.h>
 #include <fcntl.h>
-#include "mcp23017.h"
+#include <stdint.h>
+
+#include <iostream>
 
 Mcp23017::Mcp23017(int bus, int addr) : I2c(bus, addr, O_RDWR) {}
 
 int Mcp23017::clear_settings() {
-    /* Reset dir regs */
-    if (this->write_data(MCP_DIRA_REG, 0xFF) ||
-        this->write_data(MCP_DIRB_REG, 0xFF))
-        return -EIO;
+  /* Reset dir regs */
+  if (this->write_data(MCP_DIRA_REG, 0xFF) ||
+      this->write_data(MCP_DIRB_REG, 0xFF))
+    return -EIO;
 
-    /* Reset GPIO regs */
-    if (this->write_data(MCP_GPIOA_REG, 0x00) ||
-        this->write_data(MCP_GPIOB_REG, 0x00))
-        return -EIO;
+  /* Reset GPIO regs */
+  if (this->write_data(MCP_GPIOA_REG, 0x00) ||
+      this->write_data(MCP_GPIOB_REG, 0x00))
+    return -EIO;
 
-    return 0;
+  return 0;
 }
 
 int Mcp23017::begin(const uint8_t directions[]) {
-    int rc;
+  int rc;
 
-    if (!this->is_open()) {
-        rc = this->open_i2c();
-        if (rc)
-            return rc;
-    }
+  if (!this->is_open()) {
+    rc = this->open_i2c();
+    if (rc) return rc;
+  }
 
-    rc = this->clear_settings();
-    if (rc) {
-        std::cerr << "Error clearing settings\n";
-        return rc;
-    }
+  rc = this->clear_settings();
+  if (rc) {
+    std::cerr << "Error clearing settings\n";
+    return rc;
+  }
 
-    for (int i = 0; i < MCP_NUM_PINS; i++)
-        this->set_dir(i, directions[i]);
-    
-    for (int i = 0; i < MCP_NUM_PINS; i++) {
-        if (this->get_dir(i) != directions[i]) {
-            std::cerr << "Error setting direction of pin " << i << "\n";
-            return -EIO;
-        }
+  for (int i = 0; i < MCP_NUM_PINS; i++) this->set_dir(i, directions[i]);
+
+  for (int i = 0; i < MCP_NUM_PINS; i++) {
+    if (this->get_dir(i) != directions[i]) {
+      std::cerr << "Error setting direction of pin " << i << "\n";
+      return -EIO;
     }
-    return 0;
+  }
+  return 0;
 }
 
 uint8_t Mcp23017::get_dir(int pin) {
-    uint8_t current_status;
-    uint8_t dirReg;
+  uint8_t current_status;
+  uint8_t dirReg;
 
-    if (pin >= MCP_NUM_PINS)
-        return -EINVAL;
+  if (pin >= MCP_NUM_PINS) return -EINVAL;
 
-    dirReg = IS_BANK_A(pin) ? MCP_DIRA_REG : MCP_DIRB_REG;
-    pin = GET_REL_PIN(pin);
+  dirReg = IS_BANK_A(pin) ? MCP_DIRA_REG : MCP_DIRB_REG;
+  pin = GET_REL_PIN(pin);
 
-    current_status = this->read_from_reg(dirReg);
+  current_status = this->read_from_reg(dirReg);
 
-    return ((current_status >> pin) & 1);
+  return ((current_status >> pin) & 1);
 }
 
 int Mcp23017::set_dir(int pin, uint8_t dir) {
-    uint8_t current_status;
-    uint8_t dirReg;
-    int rc;
+  uint8_t current_status;
+  uint8_t dirReg;
+  int rc;
 
-    if (pin >= MCP_NUM_PINS)
-        return -EINVAL;
+  if (pin >= MCP_NUM_PINS) return -EINVAL;
 
-    dirReg = IS_BANK_A(pin) ? MCP_DIRA_REG : MCP_DIRB_REG;
-    pin = GET_REL_PIN(pin);
+  dirReg = IS_BANK_A(pin) ? MCP_DIRA_REG : MCP_DIRB_REG;
+  pin = GET_REL_PIN(pin);
 
-    current_status = this->read_from_reg(dirReg);
+  current_status = this->read_from_reg(dirReg);
 
-    if (dir)
-        rc = this->write_data(dirReg, current_status | (1 << pin));
-    else
-        rc = this->write_data(dirReg, current_status & ~(1 << pin));
+  if (dir)
+    rc = this->write_data(dirReg, current_status | (1 << pin));
+  else
+    rc = this->write_data(dirReg, current_status & ~(1 << pin));
 
-    return rc;
+  return rc;
 }
 
 uint8_t Mcp23017::get_state(int pin) {
-    uint8_t current_status;
-    uint8_t stateReg;
+  uint8_t current_status;
+  uint8_t stateReg;
 
-    if (pin >= MCP_NUM_PINS)
-        return -EINVAL;
+  if (pin >= MCP_NUM_PINS) return -EINVAL;
 
-    stateReg = IS_BANK_A(pin) ? MCP_GPIOA_REG : MCP_GPIOB_REG;
-    pin = GET_REL_PIN(pin);
+  stateReg = IS_BANK_A(pin) ? MCP_GPIOA_REG : MCP_GPIOB_REG;
+  pin = GET_REL_PIN(pin);
 
-    current_status = this->read_from_reg(stateReg);
+  current_status = this->read_from_reg(stateReg);
 
-    return ((current_status >> pin) & 1);
+  return ((current_status >> pin) & 1);
 }
 
 int Mcp23017::set_state(int pin, uint8_t val) {
-    uint8_t current_status;
-    uint8_t current_dir;
-    uint8_t stateReg;
-    uint8_t dirReg;
-    int rc;
+  uint8_t current_status;
+  uint8_t current_dir;
+  uint8_t stateReg;
+  uint8_t dirReg;
+  int rc;
 
-    if (pin >= MCP_NUM_PINS)
-        return -EINVAL;
+  if (pin >= MCP_NUM_PINS) return -EINVAL;
 
-    dirReg = IS_BANK_A(pin) ? MCP_DIRA_REG : MCP_DIRB_REG;
-    stateReg = IS_BANK_A(pin) ? MCP_GPIOA_REG : MCP_GPIOB_REG;
-    pin = GET_REL_PIN(pin);
+  dirReg = IS_BANK_A(pin) ? MCP_DIRA_REG : MCP_DIRB_REG;
+  stateReg = IS_BANK_A(pin) ? MCP_GPIOA_REG : MCP_GPIOB_REG;
+  pin = GET_REL_PIN(pin);
 
-    current_dir = this->read_from_reg(dirReg);
+  current_dir = this->read_from_reg(dirReg);
 
-    if ((current_dir >> pin) & 0x1)
-        return -EINVAL;
-    current_status = this->read_from_reg(stateReg);
+  if ((current_dir >> pin) & 0x1) return -EINVAL;
+  current_status = this->read_from_reg(stateReg);
 
-    if (val)
-        rc = this->write_data(stateReg, current_status | (1 << pin));
-    else
-        rc = this->write_data(stateReg, current_status & ~(1 << pin));
+  if (val)
+    rc = this->write_data(stateReg, current_status | (1 << pin));
+  else
+    rc = this->write_data(stateReg, current_status & ~(1 << pin));
 
-    return rc;
+  return rc;
 }

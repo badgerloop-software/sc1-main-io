9723da45403b44a7c38cf53f30a1b97f33644f25
Marco Kurzynski <mkurzynski@wisc.edu>
2021-06-26 18:36:05 -0500

    CAN object implementation

---
+++ embedded/drivers/include/can.h
@@ -0,0 +1,42 @@
+#ifndef __CAN_H__
+#define __CAN_H__
+
+#include <linux/can.h>
+#include <net/if.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#ifdef USE_VCAN
+#define CAN_INTF "vcan0"
+#else
+#define CAN_INTF "can0"
+#endif
+
+extern volatile bool NEW_CAN_MESSAGE;
+
+void updateNewMessage(int signum) { NEW_CAN_MESSAGE = true; }
+
+class CAN {
+    private:
+        struct sockaddr_can addr;
+        struct ifreq ifr;
+        int socket;
+        const struct itimerval new_val = {
+            { 0, 10000 },
+            { 0, 10000 }
+        };
+
+    protected:
+        CAN(int* s);
+        ~CAN();
+        int begin();
+        int canRead(struct can_frame* can_mesg);
+        int canSend(uint32_t id, uint8_t* data, uint8_t size);
+};
+
+#endif

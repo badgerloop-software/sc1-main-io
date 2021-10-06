09cc2a87cd52ddc1cb5da867e692227e21458960
Ben Everson <bennye3797@gmail.com>
2021-10-05 19:11:19 -0500

    Define state machine classes

--- embedded/FSM/include/StateMachine.h
+++ embedded/FSM/include/StateMachine.h
@@ -1,1 +1,31 @@
+#ifndef __STATE_MACHINE_H__
+#define __STATE_MACHINE_H__
 
+#define MAX_TRANSITIONS 100
+
+class Transition {
+ private:
+  State nextState;
+
+ public:
+  Transition(State nextState, int *func());
+  State trigger();
+};
+
+class State {
+ private:
+  State getNextState();
+
+ public:
+  int addTransition();
+  Transition transitions[MAX_TRANSITIONS];
+};
+
+class StateMachine {
+ public:
+  StateMachine(State startState);
+  State currentState;
+  State goToNextState();
+};
+
+#endif

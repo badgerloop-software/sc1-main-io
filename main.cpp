54613d8d92f158ab33e21c652cfb49391f172f05
James <jrvollmer@wisc.edu>
2021-10-10 14:04:39 -0500

    Dummy state machine test and modify addTransition()

--- main.cpp
+++ main.cpp
@@ -6,43 +6,119 @@
 #include "mcp23017.h"
 
 int main() {
-  Mcp23017 rick = Mcp23017(2, 0x20);
-  Mcp23017 morty = Mcp23017(2, 0x21);
+    bool hitEStop = false;
+    int devInput1 = 0;
+    int devInput2 = 1;
 
-  rick.begin(rick_directions);
-  morty.begin(morty_directions);
+    State state1;
+    State state2;
+    State state3;
+    State eStop;
 
-  std::cout << "Rick\n";
-  for (int i = 0; i < MCP_NUM_PINS; i++) {
-    rick.set_state(i, 1);
-    usleep(1000);
-    std::cout << "Pin " << +i << " val " << (int)rick.get_state(i) << std::endl;
-  }
+    // Transition from any state to eStop
+    Transition stop = Transition(eStop, State triggerFunc() {
+        return (hitEStop ? this->nextState : NULL);
+    });
 
-  sleep(2);
+    // Set state1's transitions
+    state1->addTransition(stop);
+    state1->addTransition(Transition(state3, State triggerFunc() {
+        return ((devInput1 < devInput2) ? this->nextState : NULL);
+    }));
+    state1->addTransition(Transition(state2, State triggerFunc() {
+        return ((devInput1 == devInput2) ? this->nextState : NULL);
+    }));
+    state1->addTransition(Transition(state1, State triggerFunc() {
+        return ((devInput1 > devInput2) ? this->nextState : NULL);
+    }));
 
-  for (int i = 0; i < MCP_NUM_PINS; i++) {
-    rick.set_state(i, 0);
-    usleep(1000);
-    std::cout << "Pin " << +i << " val " << (int)rick.get_state(i) << std::endl;
-  }
+    // Set state2's transitions
+    state2->addTransition(stop);
+    state2->addTransition(Transition(state3, State triggerFunc() {
+        return ((devInput1 < devInput2) ? this->nextState : NULL);
+    }));
+    state2->addTransition(Transition(state1, State triggerFunc() {
+        return (((devInput1 > devInput2) || (devInput1 == devInput2)) ? this->nextState : NULL);
+    }));
 
-  std::cout << "Morty\n";
-  for (int i = 0; i < MCP_NUM_PINS; i++) {
-    morty.set_state(i, 1);
-    usleep(1000);
-    std::cout << "Pin " << +i << " val " << (int)morty.get_state(i)
-              << std::endl;
-  }
+    // Set state3's transitions
+    state3->addTransition(stop);
+    state3->addTransition(Transition(state2, State triggerFunc() {
+        return ((devInput1 == devInput2) ? this->nextState : NULL);
+    }));
+    state3->addTransition(Transition(state3, State triggerFunc() {
+        return (((devInput1 > devInput2) || (devInput1 < devInput2)) ? this->nextState : NULL);
+    }));
 
-  sleep(2);
 
-  for (int i = 0; i < MCP_NUM_PINS; i++) {
-    morty.set_state(i, 0);
-    usleep(1000);
-    std::cout << "Pin " << +i << " val " << (int)morty.get_state(i)
-              << std::endl;
-  }
+    StateMachine machine1 = StateMachine(state1);
 
-  return 0;
+    // Should start at state1
+    printf(machine1->currentState == state1);
+
+    // state1 to state2
+    devInput1 = 1;
+    devInput2 = 1;
+    machine1->goToNextState();
+    printf(machine1->currentState == state2);
+
+    // state2 to state3
+    devInput1 = 0;
+    devInput2 = 1;
+    machine1->goToNextState();
+    printf(machine1->currentState == state3);
+
+    // state3 to state3
+    machine1->goToNextState();
+    printf(machine1->currentState == state3);
+
+    // state3 to state3
+    devInput1 = 1;
+    devInput2 = 0;
+    machine1->goToNextState();
+    printf(machine1->currentState == state3);
+
+    // state3 to state2
+    devInput1 = 1;
+    devInput2 = 1;
+    machine1->goToNextState();
+    printf(machine1->currentState == state2);
+
+    // state2 to state1
+    machine1->goToNextState();
+    printf(machine1->currentState == state1);
+
+    // state1 to state3
+    devInput1 = 0;
+    devInput2 = 1;
+    machine1->goToNextState();
+    printf(machine1->currentState == state3);
+
+    // state3 to state2
+    devInput1 = 1;
+    devInput2 = 1;
+    machine1->goToNextState();
+    printf(machine1->currentState == state2);
+
+    // state2 to state1
+    devInput1 = 1;
+    devInput2 = 0;
+    machine1->goToNextState();
+    printf(machine1->currentState == state1);
+
+    // state1 to state1
+    devInput1 = 1;
+    devInput2 = 0;
+    machine1->goToNextState();
+    printf(machine1->currentState == state1);
+
+    // state1 to eStop
+    hitEStop = true;
+    machine1->goToNextState();
+    printf(machine1->currentState == eStop);
+
+    // eStop has no transitions, but machine1 should stay in eStop
+    machine1->goToNextState();
+    printf(machine1->currentState == eStop);
+
 }

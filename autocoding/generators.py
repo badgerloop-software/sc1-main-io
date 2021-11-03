51dd4a6e22a87058dd66636d9886dfe4edd9bc6f
ykalra <ykalra@wisc.edu>
2021-11-03 17:20:54 -0500

    Added function for Triggers header

--- autocoding/generators.py
+++ autocoding/generators.py
@@ -1,71 +1,106 @@
 import util
 
-# EXAMPLE DATA_H GENERATOR
-#
-# # Generates the data.h file
-# def generateDataHeader(data):
-#     headers = ""
-#     out = ""
-#     # Generate initializers
-#     for field in data.iter("struct"):
-#         if "init" not in field.attrib:
-#             continue
-#         out += "int " + field.attrib["init"] + "(void);\n"
+# # EXAMPLE DATA_H GENERATOR
+# #
+# # # Generates the data.h file
+def generateDataHeader(data):
+    headers = ""
+    out = ""
+    # Generate initializers
+    for field in data.iter("struct"):
+        if "init" not in field.attrib:
+            continue
+        out += "int " + field.attrib["init"] + "(void);\n"
 
-#     out += "\n"
-#     # Generate data structures
-#     for struct in data.iter("struct"):
-#         if "comment" in struct.attrib:
-#             out += util.generateComment(struct.attrib["comment"], 100)
-#         out += "typedef struct " + struct.attrib["id"] + "_t {\n"
+    out += "\n"
+    # Generate data structures
+    for struct in data.iter("struct"):
+        if "comment" in struct.attrib:
+            out += util.generateComment(struct.attrib["comment"], 100)
+        out += "typedef struct " + struct.attrib["id"] + "_t {\n"
 
-#         # Semaphores
-#         out += "\tpthread_mutex_t " + struct.attrib["id"] + "Mutex;\n"
+        # Semaphores
+        out += "\tpthread_mutex_t " + struct.attrib["id"] + "Mutex;\n"
 
-#         for field in struct:
-#             if "comment" in field.attrib:
-#                 out += util.generateComment(field.attrib["comment"], 100, "\t")
-#             fieldId = (
-#                 "*"
-#                 if ("pointer" in field.attrib and field.attrib["pointer"] == "true")
-#                 else ""
-#             ) + field.attrib["id"]
-#             if field.tag == "struct":
-#                 out += "\tstruct " + field.attrib["id"] + "_t " + fieldId + ";\n"
-#             elif field.tag == "field":
-#                 fieldType = field.attrib["type"]
-#                 # Deal with arrays
-#                 if field.attrib["type"].endswith("]"):
-#                     fieldId += "[" + field.attrib["type"].split("[")[1]
-#                     fieldType = field.attrib["type"].split("[")[0]
-#                 out += "\t" + fieldType + " " + fieldId + ";\n"
+        for field in struct:
+            if "comment" in field.attrib:
+                out += util.generateComment(field.attrib["comment"], 100, "\t")
+            fieldId = (
+                "*"
+                if ("pointer" in field.attrib and field.attrib["pointer"] == "true")
+                else ""
+            ) + field.attrib["id"]
+            if field.tag == "struct":
+                out += "\tstruct " + field.attrib["id"] + "_t " + fieldId + ";\n"
+            elif field.tag == "field":
+                fieldType = field.attrib["type"]
+                # Deal with arrays
+                if field.attrib["type"].endswith("]"):
+                    fieldId += "[" + field.attrib["type"].split("[")[1]
+                    fieldType = field.attrib["type"].split("[")[0]
+                out += "\t" + fieldType + " " + fieldId + ";\n"
 
-#                 # Create getter and setter headers
-#                 headers += util.generateComment("Getter and setter for " + struct.attrib["id"] + "->" + field.attrib["id"], 100)
-#                 fieldType = field.attrib["type"]
-#                 isArray = False
-#                 if "[" in field.attrib["type"]:
-#                     fieldType = fieldType.split("[")[0]
-#                     isArray = True
-#                 headers += fieldType + " get" + util.capitalize(struct.attrib["id"]) + util.capitalize(field.attrib["id"]) + "();\n"
-#                 headers += "void set" + util.capitalize(struct.attrib["id"]) + util.capitalize(field.attrib["id"]) + "(" + fieldType + " val);\n\n"
+                # Create getter and setter headers
+                headers += util.generateComment(
+                    "Getter and setter for "
+                    + struct.attrib["id"]
+                    + "->"
+                    + field.attrib["id"],
+                    100,
+                )
+                fieldType = field.attrib["type"]
+                isArray = False
+                if "[" in field.attrib["type"]:
+                    fieldType = fieldType.split("[")[0]
+                    isArray = True
+                headers += (
+                    fieldType
+                    + " get"
+                    + util.capitalize(struct.attrib["id"])
+                    + util.capitalize(field.attrib["id"])
+                    + "();\n"
+                )
+                headers += (
+                    "void set"
+                    + util.capitalize(struct.attrib["id"])
+                    + util.capitalize(field.attrib["id"])
+                    + "("
+                    + fieldType
+                    + " val);\n\n"
+                )
 
-#                 headers += (
-#                     fieldType
-#                     + " "
-#                     + util.getGetReference(struct, field)
-#                     + "("
-#                     + ("int index" if isArray else "")
-#                     + ");\n"
-#                 )
-#                 headers += (
-#                     "void "
-#                     + util.getSetReference(struct, field)
-#                     + "("
-#                     + fieldType
-#                     + " val"
-#                     + (", int index" if isArray else "")
-#                     + ");\n\n"
-#                 )
-#         out += "} " + struct.attrib["id"] + "_t;\n\n"
-#     return out + "\n" + headers + "\n\n\n"
+                headers += (
+                    fieldType
+                    + " "
+                    + util.getGetReference(struct, field)
+                    + "("
+                    + ("int index" if isArray else "")
+                    + ");\n"
+                )
+                headers += (
+                    "void "
+                    + util.getSetReference(struct, field)
+                    + "("
+                    + fieldType
+                    + " val"
+                    + (", int index" if isArray else "")
+                    + ");\n\n"
+                )
+        out += "} " + struct.attrib["id"] + "_t;\n\n"
+    return out + "\n" + headers + "\n\n\n"
+
+
+def generateTriggersHeader(Triggers):
+    out = ""
+    for extern in Triggers.iter("extern"):
+        out += "extern " + extern.attrib["type"] + " " + extern.attrib["id"] + ";\n"
+    out += "State* eStopTrigger();\n"
+    for trigger in Triggers.iter("trigger"):
+        out += (
+            "State* state"
+            + trigger.attrib["currState"]
+            + "To"
+            + trigger.attrib["nextState"]
+            + "Trigger();\n"
+        )
+    return out

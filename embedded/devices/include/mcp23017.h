0feae099f421de0773a969bf94e82c94abe395d2
Olivia <odsouza@wisc.edu>
2021-11-05 17:01:18 -0500

    removed extra whitespace, formatted test call, wrote test pins to 0

--- embedded/devices/include/mcp23017.h
+++ embedded/devices/include/mcp23017.h
@@ -1,24 +1,24 @@
 #ifndef __mcp__h__
 #define __mcp__h__
-#include <stdint.h>
-
 #include "i2c.h"
+#include "stdint.h"
 
 #define MCP_DIRA_REG 0x00
 #define MCP_DIRB_REG 0x01
 #define MCP_GPIOA_REG 0x12
-define MCP_GPIOB_REG 0x13
+#define MCP_GPIOB_REG 0x13
 
 #define MCP_NUM_PINS 16
 
 #define IS_BANK_A(pin) (pin < (MCP_NUM_PINS >> 1))
 #define GET_REL_PIN(pin) (pin & 0x7)
 
-    static const uint8_t rick_directions[16] = {0, 0, 0, 0, 0, 0, 0, 0,
-                                                0, 0, 0, 0, 0, 0, 0, 0};
+static const uint8_t rick_directions[16] = {0, 0, 0, 0, 0, 0, 0, 0,
+                                            0, 0, 0, 0, 0, 0, 0, 0};
 
 static const uint8_t morty_directions[16] = {0, 0, 0, 0, 0, 0, 0, 0,
                                              0, 0, 0, 0, 0, 0, 0, 0};
+
 class Mcp23017 : private I2c {
  private:
   int clear_settings();
--- tests.cpp
+++ tests.cpp
@@ -8,16 +8,18 @@ using namespace std;
 
 // Test functions go here
 int mcp23017_test() {
+  static const uint8_t test_array[16] = {0, 1, 0, 0, 1, 0, 1, 1,
+                                         0, 1, 0, 0, 0, 1, 1, 1};
   // initialize the device
   Mcp23017 dev = Mcp23017(2, 0x24);
   // begin the device dev.begin(array of 16 bits)
-  if (dev.begin(random_array)) return 1;
+  if (dev.begin(test_array)) return 1;
   // write one output to bank a, and one to bank b
-  dev.set_state(3, 1);
-  dev.set_state(11, 1);
-
-  // read from pin that was written to
+  // and read from pin that was written to
+  dev.set_state(3, 0);
   if (dev.get_state(3) != 1) return 1;
+
+  dev.set_state(11, 0);
   if (dev.get_state(11) != 1) return 1;
 
   return 0;
@@ -30,10 +32,10 @@ int main() {
   serial.writeString("mcp23017");
 
   // Test calls go here
-  if (int mcp23017_test())
-    return 0;
-  else
-    std::cout << "MCP23017 test failed\n";
+  if (!mcp23017_test()) {
+    std::cout << "MCP23017 test failed/n";
+    return 1;
+  }
 
   serial.closeDevice();
   return 0;

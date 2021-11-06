2e52450b1249d4d7fd69bce24c4f06effd3518a6
Ben Everson <bennye3797@gmail.com>
2021-11-06 10:33:27 -0500

    Revert main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,45 @@
 
 #include "i2c.h"
 #include "mcp23017.h"
-#include "serial.h"
 
 int main() {
-  Serial serial = Serial();
-  if (serial.openDevice(4, 115200) != 1) std::cout << "error\n";
-  char buff[1];
+  Mcp23017 rick = Mcp23017(2, 0x20);
+  Mcp23017 morty = Mcp23017(2, 0x21);
 
-  serial.writeString("Hello world");
+  rick.begin(rick_directions);
+  morty.begin(morty_directions);
+
+  std::cout << "Rick\n";
+  for (int i = 0; i < MCP_NUM_PINS; i++) {
+    rick.set_state(i, 1);
+    usleep(1000);
+    std::cout << "Pin " << +i << " val " << (int)rick.get_state(i) << std::endl;
+  }
+
+  sleep(2);
+
+  for (int i = 0; i < MCP_NUM_PINS; i++) {
+    rick.set_state(i, 0);
+    usleep(1000);
+    std::cout << "Pin " << +i << " val " << (int)rick.get_state(i) << std::endl;
+  }
+
+  std::cout << "Morty\n";
+  for (int i = 0; i < MCP_NUM_PINS; i++) {
+    morty.set_state(i, 1);
+    usleep(1000);
+    std::cout << "Pin " << +i << " val " << (int)morty.get_state(i)
+              << std::endl;
+  }
+
+  sleep(2);
+
+  for (int i = 0; i < MCP_NUM_PINS; i++) {
+    morty.set_state(i, 0);
+    usleep(1000);
+    std::cout << "Pin " << +i << " val " << (int)morty.get_state(i)
+              << std::endl;
+  }
+
   return 0;
 }

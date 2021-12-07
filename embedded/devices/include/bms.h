685d0a9e4b3bc36306c028788e8dc3bd057a3da8
bquan0 <powerbowen@gmail.com>
2021-12-07 17:50:08 -0600

    battery management system code

---
+++ embedded/devices/include/bms.h
@@ -0,0 +1,98 @@
+#include <linux/can.h>
+#include <pthread.h>
+
+#include "can.h"
+
+class Bms {
+ private:
+  pthread_mutex_t bmsMutex;
+  float packCurrent;
+  float packVoltage;
+  int imdStatus;
+  uint16_t packDCL;
+  int16_t packCCL;
+  uint16_t packResistance;
+  uint8_t packHealth;
+  float packOpenVoltage;
+  uint16_t packCycles;
+  uint16_t packAh;
+  float inputVoltage;
+  uint8_t Soc;
+  uint16_t relayStatus;
+  uint8_t highTemp;
+  uint8_t lowTemp;
+  uint8_t avgTemp;
+  float cellMaxVoltage;
+  float cellMinVoltage;
+  uint16_t cellAvgVoltage;
+  uint8_t maxCells;
+  uint8_t numCells;
+
+ public:
+  Bms();
+  int bmsClearFaults(void);  // write function
+  int bmsParseMsg();         // read function
+
+  float getBmsPackCurrent();
+  void setBmsPackCurrent(float val);
+
+  float getBmsPackVoltage();
+  void setBmsPackVoltage(float val);
+
+  int getBmsImdStatus();
+  void setBmsImdStatus(int val);
+
+  uint16_t getBmsPackDCL();
+  void setBmsPackDCL(uint16_t val);
+
+  int16_t getBmsPackCCL();
+  void setBmsPackCCL(int16_t val);
+
+  uint16_t getBmsPackResistance();
+  void setBmsPackResistance(uint16_t val);
+
+  uint8_t getBmsPackHealth();
+  void setBmsPackHealth(uint8_t val);
+
+  float getBmsPackOpenVoltage();
+  void setBmsPackOpenVoltage(float val);
+
+  uint16_t getBmsPackCycles();
+  void setBmsPackCycles(uint16_t val);
+
+  uint16_t getBmsPackAh();
+  void setBmsPackAh(uint16_t val);
+
+  float getBmsInputVoltage();
+  void setBmsInputVoltage(float val);
+
+  uint8_t getBmsSoc();
+  void setBmsSoc(uint8_t val);
+
+  uint16_t getBmsRelayStatus();
+  void setBmsRelayStatus(uint16_t val);
+
+  uint8_t getBmsHighTemp();
+  void setBmsHighTemp(uint8_t val);
+
+  uint8_t getBmsLowTemp();
+  void setBmsLowTemp(uint8_t val);
+
+  uint8_t getBmsAvgTemp();
+  void setBmsAvgTemp(uint8_t val);
+
+  float getBmsCellMaxVoltage();
+  void setBmsCellMaxVoltage(float val);
+
+  float getBmsCellMinVoltage();
+  void setBmsCellMinVoltage(float val);
+
+  uint16_t getBmsCellAvgVoltage();
+  void setBmsCellAvgVoltage(uint16_t val);
+
+  uint8_t getBmsMaxCells();
+  void setBmsMaxCells(uint8_t val);
+
+  uint8_t getBmsNumCells();
+  void setBmsNumCells(uint8_t val);
+};
---
+++ embedded/devices/src/bms.cpp
@@ -0,0 +1,380 @@
+#include "bms.h"
+
+int bmsClearFaults(void)  // send CAN info
+{
+  uint16_t can_id = 0x7e3;
+  uint8_t TxData[8];          // TODO: find out why this is diff
+  uint8_t length = 16 /*8*/;  // TODO p2: than this
+
+  TxData[0] = 0x01;
+  TxData[1] = 0x04;
+  TxData[2] = 0x00;
+  TxData[3] = 0x00;
+  TxData[4] = 0x00;
+  TxData[5] = 0x01;
+  TxData[6] = 0x00;
+  TxData[7] = 0x00;
+
+  canSend(can_id, TxData, length);
+
+  return 0;
+}
+
+int bmsParseMsg()  // read CAN bus
+{
+  struct can_frame frame;
+  canRead(*frame);  // read CAN bus to get id
+  uint32_t id = frame.can_id;
+  uint8_t* msg = *frame;
+
+  switch (id) {
+    case 0x6B0:
+      setBmsPackCurrent((msg[1] | msg[0] << 8) / 10);
+      setBmsPackVoltage((msg[3] | msg[2] << 8) / 10);
+      setBmsSoc(msg[4] / 2);
+      setBmsRelayStatus(msg[6] | msg[5] << 8);
+      setBmsCellMaxVoltage(((msg[5] << 8) | msg[6]) / 10000.0);
+
+      break;
+    case 0x6B1:
+      setBmsPackDCL(msg[1] | msg[0] << 8);
+      setBmsHighTemp(msg[4]);
+      /*			setBmsCellMinVoltage((msg[7] | (msg[6] << 8)) /
+       * 10000.0;)*/
+      break;
+    case 0x653:
+      /*			printf("ID: 0x%3lx\r\n", (long unsigned int)
+       * id);*/
+      setBmsRelayStatus(msg[1] | msg[0] << 8);
+      setBmsRelayStatus(msg[0]);
+      setBmsInputVoltage((msg[2] | (msg[3] << 8)) / 10);
+      break;
+    case 0x652:
+
+      setBmsPackCCL(msg[0] | (msg[1] << 8));
+      setBmsPackDCL(msg[2] | (msg[3] << 8));
+      /*			setBmsCellMaxVoltage(msg[4] | (msg[5] << 8));*/
+      /*			setBmsCellMaxVoltage(getBmsCellMaxVoltage() /
+       * 10000);*/
+      /*			setBmsCellMinVoltage(msg[6] | (msg[7] << 8));*/
+      /*			setBmsCellMinVoltage(getBmsCellMinVoltage() /
+       * 10000);*/
+      break;
+    case 0x651:
+      /*			setBmsCellMaxVoltage(msg[2] | (msg[3] << 8));*/
+      /*			setBmsCellMinVoltage(msg[0] | (msg[1] << 8));*/
+      /*			setBmsCellAvgVoltage(msg[5] | (msg[4] << 8));*/
+      /*			setBmsCellAvgVoltage(getBmsCellAvgVoltage() /
+       * 1000);*/
+      /*          setBmsCellMaxVoltage(getBmsCellMaxVoltage() / 1000);*/
+      /*          setBmsCellMinVoltage(getBmsCellMinVoltage() / 1000);*/
+      /*			setBmsCellMaxVoltage /= 1000;*/
+      /*			setBmsCellMinVoltage /= 1000;*/
+      setBmsMaxCells(msg[6]);
+      setBmsNumCells(msg[7]);
+      break;
+    case 0x650:
+
+      setBmsSoc(msg[0]);
+      setBmsSoc(getBmsSoc() / 2);
+      setBmsPackResistance(msg[1] | (msg[2] << 8));
+      setBmsPackHealth(msg[3]);
+      setBmsPackOpenVoltage((msg[4] | (msg[5] << 8)) / 10);
+      setBmsPackCycles(msg[6] | (msg[7] << 8));
+      break;
+    case 0x150:
+      setBmsPackAh(msg[4] | (msg[5] << 8));
+      setBmsHighTemp(msg[6]);
+      setBmsLowTemp(msg[7]);
+      break;
+    case 0x6b2:
+      setBmsCellMinVoltage(((msg[0] << 8) | msg[1]) / 10000.0);
+      /*            setBmsCellMaxVoltage(((msg[2] << 8) | msg[3]) / 10000)*/
+      setBmsAvgTemp(msg[2]);
+      setBmsImdStatus(msg[3]);
+      break;
+    case 0x80:
+      break;
+    case 0x36:
+      if (msg[0] >= 0 && msg[0] < 72)
+        cells[msg[0]] = (msg[2] | (msg[1] << 8)) / 10000.0;
+      break;
+
+    default:
+      return 0;
+  }
+  return 1;
+}
+
+float Bms::getBmsPackCurrent() {
+  pthread_mutex_lock(&this->bmsMutex);
+  float val = this->packCurrent;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackCurrent(float val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packCurrent = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+float Bms::getBmsPackVoltage() {
+  pthread_mutex_lock(&this->bmsMutex);
+  float val = this->packVoltage;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackVoltage(float val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packVoltage = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+int Bms::getBmsImdStatus() {
+  pthread_mutex_lock(&this->bmsMutex);
+  int val = this->imdStatus;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsImdStatus(int val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->imdStatus = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint16_t Bms::getBmsPackDCL() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint16_t val = this->packDCL;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackDCL(uint16_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packDCL = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+int16_t Bms::getBmsPackCCL() {
+  pthread_mutex_lock(&this->bmsMutex);
+  int16_t val = this->packCCL;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackCCL(int16_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packCCL = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint16_t Bms::getBmsPackResistance() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint16_t val = this->packResistance;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackResistance(uint16_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packResistance = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint8_t Bms::getBmsPackHealth() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint8_t val = this->packHealth;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackHealth(uint8_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packHealth = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+float Bms::getBmsPackOpenVoltage() {
+  pthread_mutex_lock(&this->bmsMutex);
+  float val = this->packOpenVoltage;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackOpenVoltage(float val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packOpenVoltage = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint16_t Bms::getBmsPackCycles() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint16_t val = this->packCycles;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackCycles(uint16_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packCycles = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint16_t Bms::getBmsPackAh() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint16_t val = this->packAh;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsPackAh(uint16_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->packAh = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+float Bms::getBmsInputVoltage() {
+  pthread_mutex_lock(&this->bmsMutex);
+  float val = this->inputVoltage;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsInputVoltage(float val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->inputVoltage = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint8_t Bms::getBmsSoc() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint8_t val = this->Soc;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsSoc(uint8_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->Soc = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint16_t Bms::getBmsRelayStatus() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint16_t val = this->relayStatus;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsRelayStatus(uint16_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->relayStatus = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint8_t Bms::getBmsHighTemp() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint8_t val = this->highTemp;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsHighTemp(uint8_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->highTemp = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint8_t Bms::getBmsLowTemp() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint8_t val = this->lowTemp;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsLowTemp(uint8_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->lowTemp = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint8_t Bms::getBmsAvgTemp() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint8_t val = this->avgTemp;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsAvgTemp(uint8_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->avgTemp = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+float Bms::getBmsCellMaxVoltage() {
+  pthread_mutex_lock(&this->bmsMutex);
+  float val = this->cellMaxVoltage;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsCellMaxVoltage(float val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->cellMaxVoltage = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+float Bms::getBmsCellMinVoltage() {
+  pthread_mutex_lock(&this->bmsMutex);
+  float val = this->cellMinVoltage;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsCellMinVoltage(float val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->cellMinVoltage = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint16_t Bms::getBmsCellAvgVoltage() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint16_t val = this->cellAvgVoltage;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsCellAvgVoltage(uint16_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->cellAvgVoltage = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint8_t Bms::getBmsMaxCells() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint8_t val = this->maxCells;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsMaxCells(uint8_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->maxCells = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}
+
+uint8_t Bms::getBmsNumCells() {
+  pthread_mutex_lock(&this->bmsMutex);
+  uint8_t val = this->numCells;
+  pthread_mutex_unlock(&this->bmsMutex);
+  return val;
+}
+
+void Bms::setBmsNumCells(uint8_t val) {
+  pthread_mutex_lock(&this->bmsMutex);
+  this->numCells = val;
+  pthread_mutex_unlock(&this->bmsMutex);
+}

a2d6ffe8f7580f0834d20a531ed419293ee48e91
Marco Kurzynski <marcokurzynski@icloud.com>
2022-03-13 20:40:28 -0500

    finished rough draft of RTC

--- embedded/devices/include/rtc.h
+++ embedded/devices/include/rtc.h
@@ -3,38 +3,57 @@
 
 #include "i2c.h"
 
-static constexpr unsigned char bit(int n) { return 1 << n; }
-constexpr unsigned char BIT0 = bit(0);
-constexpr unsigned char BIT1 = bit(1);
-constexpr unsigned char BIT2 = bit(2);
-constexpr unsigned char BIT3 = bit(3);
-constexpr unsigned char BIT4 = bit(4);
-constexpr unsigned char BIT5 = bit(5);
-constexpr unsigned char BIT6 = bit(6);
-constexpr unsigned char BIT7 = bit(7);
+static constexpr uint8_t bit(int n) { return 1 << n; }
+constexpr uint8_t BIT0 = bit(0);
+constexpr uint8_t BIT1 = bit(1);
+constexpr uint8_t BIT2 = bit(2);
+constexpr uint8_t BIT3 = bit(3);
+constexpr uint8_t BIT4 = bit(4);
+constexpr uint8_t BIT5 = bit(5);
+constexpr uint8_t BIT6 = bit(6);
+constexpr uint8_t BIT7 = bit(7);
+inline uint8_t d2bcd(uint8_t dec) {
+  return ((dec / 10 % 10) << 4) | (dec % 10);
+}
 
-/**
+/*
+ * REAL TIME CLOCK REGISTERS
+ * DEFAULT ALL: 0x0
+ * BCD
+ */
+// Second
+#define SC 0x0
+// Minute
+#define MN 0x1
+// Hour
+#define HR 0x2
+// Day
+#define DT 0x3
+// Month
+#define MO 0x4
+// Year since 2000?
+#define YR 0x5
+// Day of Week
+#define DW 0x6
+
+/*
  * STATUS REGISTER (SR)
- * ADDR: 07h
+ * DEFAULT: 0x1
  *
  * BIT0: RTCF
  *  1 after total power failure
  *  0 after first valid write to RTC
  *
  * BIT1: BAT
- *  1 when enters battery backup mode
+ *  1 when enters BBM (Battery Backup Mode)
  *  0 if ARST bit or manually via SR
  *
  * BIT2: ALM
  *  1 if alarm matches RTC
  *  0 if ARST bit or manually via SR
  *
- * BIT3: reserved
- *
  * BIT4: WRTC
  *  1 to enable RTC
- *
- * BIT5: reserved
  *
  * BIT6: XTOSCB
  *  1 DISABLES crystal,
@@ -44,12 +63,95 @@ constexpr unsigned char BIT7 = bit(7);
  *  1 enables reset after valid read of SR
  *  0 and user must manually reset BAT and ALM
  */
-#define SR 0x7h
+#define SR 0x7
+
+/*
+ * INTERRUPT CONTROL REGISTER (INT)
+ * DEFAULT: 0x0
+ *
+ * BIT3-BIT0: FO
+ *  See TABLE 4
+ *
+ * BIT4: FOBATB
+ *  1 disables output during BBM
+ *  0 enables output during BBM
+ *
+ * BIT5: LPMODE
+ *  1 low power mode
+ *  0 normal mode
+ *
+ * BIT6: ALME
+ *  1 enables alarm
+ *  0 disables alarm
+ *
+ * BIT7: IM
+ *  1 recurring alarm
+ *  0 single event alarm
+ *   low until ALM bit cleared
+ *   !!OUTPUT IS ACTIVE LOW!!
+ */
+#define INT 0x8
+
+/*
+ * ANALOG TRIMMING REGISTER (ATR)
+ * DEFAULT: 0x0
+ *
+ * BIT5-BIT0: Trimming Registers
+ *  See eq 2
+ *
+ * BIT7-BIT6: Battery Mode ATR Selection
+ *  0x0  0.0pF
+ *  0x1 -0.5pF
+ *  0x2 +0.5pF
+ *  0x3 +1.0pF
+ *
+ */
+#define ATR 0xA
+
+/*
+ * DIGITAL TRIMING REGISTER (DTR)
+ * DEFAULT: 0x0
+ */
+
+/* BIT2: Sign bit
+ *  0 is frequency compensation > 0
+ *  1 is frequency compensation < 0
+ *
+ * BIT1-BIT0: magnitude
+ *  BIT1 adjusts 40ppm
+ *  BIT0 adjusts 20ppm
+ *
+ */
+#define DTR 0xB
+
+/*
+ * ALARM REGISTERS
+ * DEFAULT ALL: 0x0
+ * BCD
+ */
+// Second
+#define SCA 0xC
+// Minute
+#define MNA 0xD
+// Hour
+#define HRA 0xE
+// Day
+#define DTA 0xF
+// Month
+#define MOA 0x10
+// Day of Week
+#define DWA 0x11
 
 class RTC : private I2c {
  public:
   RTC(int bus, int addr);
   int begin(void);
+  int setAlarm(uint8_t dwa, uint8_t moa, uint8_t dta, uint8_t hra, uint8_t mna,
+               uint8_t sca, bool recur);
+
+ private:
+  int setRTC(uint8_t dw, uint8_t yr, uint8_t mo, uint8_t dt, uint8_t hr,
+             uint8_t mn, uint8_t sc);
 };
 
 #endif
--- embedded/devices/src/rtc.cpp
+++ embedded/devices/src/rtc.cpp
@@ -1,7 +1,50 @@
 #include "rtc.h"
 
+#include <errno.h>
 #include <fcntl.h>
+
+#include <chrono>
+#include <iostream>
+#include <numeric>
 
 RTC::RTC(int bus, int addr) : I2c(bus, addr, O_RDWR) {}
 
-int RTC::begin(void) { return open_i2c(); }
+int RTC::begin(void) {
+  if (open_i2c() || write_data(SR, (BIT7 | BIT4))) return -EIO;
+
+  std::time_t time =
+      std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+  std::tm date = *gmtime(&time);
+  if (setRTC(d2bcd(date.tm_wday), d2bcd(date.tm_year - 100),
+             d2bcd(date.tm_mon + 1), d2bcd(date.tm_mday), d2bcd(date.tm_hour),
+             d2bcd(date.tm_min), d2bcd(date.tm_sec > 59 ? 59 : date.tm_sec)))
+    return -EIO;
+
+  while (read_from_reg(SR) & BIT0) {
+  }
+  return 0;
+}
+
+int RTC::setAlarm(uint8_t dwa, uint8_t moa, uint8_t dta, uint8_t hra,
+                  uint8_t mna, uint8_t sca, bool recur) {
+  if (read_from_reg(SR) & BIT1) {
+    std::cout << "Entered BBM" << std::endl;
+    // reading sr clears ALM and BAT,
+    // but should we error if in battery backup mode?
+  }
+  write_data(INT, ((recur ? BIT7 : 0x0) | BIT6));
+  if (write_data(DWA, dwa) || write_data(MOA, moa) || write_data(DTA, dta) ||
+      write_data(HRA, hra) || write_data(MNA, mna) || write_data(SCA, sca))
+    return -EIO;
+  return 0;
+}
+
+int RTC::setRTC(uint8_t dw, uint8_t yr, uint8_t mo, uint8_t dt, uint8_t hr,
+                uint8_t mn, uint8_t sc) {
+  if (write_data(DW, dw) || write_data(YR, yr) || write_data(MO, mo) ||
+      write_data(DT, dt) || write_data(HR, hr) || write_data(MN, mn) ||
+      write_data(SC, sc))
+    return -EIO;
+  return 0;
+}

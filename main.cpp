98cba649f1e052ab4ce44ff664d9e71980f63695
Wilson Guo <wigu701@gmail.com>
2022-12-06 17:19:31 -0600

    MCC CAN message monitoring code

--- main.cpp
+++ main.cpp
@@ -1,62 +1,75 @@
 #include <unistd.h>
 
+#include <iomanip>
 #include <iostream>
 
-#include "i2c.h"
-#include "ina3221.h"
-#include "mcp23017.h"
+#include "can.h"
 
+void printTable(uint16_t curGPIO, float curRPM, float curAccel,
+                float curBrake) {
+  // Wipe screen
+  std::cout << "\033[2J\033[1;1H";
+
+  // Table printout
+  std::cout << "  Signal Name  |  Value" << std::endl;
+  std::cout << "  RPM          |  " << curRPM << std::endl;
+  std::cout << "  Accel V      |  " << curAccel << std::endl;
+  std::cout << "  Brake V      |  " << curBrake << std::endl;
+  std::cout << "  Power        |  " << (((curGPIO >> 0) & 0x1) ? "On" : "Off")
+            << std::endl;
+  std::cout << "  Direction    |  "
+            << (((curGPIO >> 1) & 0x1) ? "Forward" : "Reverse") << std::endl;
+  std::cout << "  Brake        |  " << (((curGPIO >> 2) & 0x1) ? "On" : "Off")
+            << std::endl;
+  std::cout << "  Eco          |  "
+            << (((curGPIO >> 3) & 0x1) ? "Eco" : "Sport") << std::endl;
+  std::cout << "  Crz Enable   |  " << (((curGPIO >> 4) & 0x1) ? "On" : "Off")
+            << std::endl;
+  std::cout << "  Crz Mode     |  " << (((curGPIO >> 5) & 0x1) ? "B" : "A")
+            << std::endl;
+  std::cout << "  MC Status    |  " << (((curGPIO >> 6) & 0x1) ? "Error" : "")
+            << std::endl;
+}
+
+// Monitor CAN Bus for messages from the MCC
+// Update current on screen values on arrival
 int main() {
-  // Mcp23017 rick = Mcp23017(2, 0x20);
-  // Mcp23017 morty = Mcp23017(2, 0x21);
+  // Define variables
+  std::setprecision(3);
+  uint16_t curGPIO = 0;
+  float curRPM = 0;
+  float curAccel = 0;
+  float curBrake = 0;
 
-  // rick.begin(rick_directions);
-  // morty.begin(morty_directions);
+  // Initialize bus
+  Can busMonitor;
+  busMonitor.init();
+  struct can_frame newMessage;
 
-  // std::cout << "Rick\n";
-  // for (int i = 0; i < MCP_NUM_PINS; i++) {
-  //   rick.set_state(i, 1);
-  //   usleep(1000);
-  //   std::cout << "Pin " << +i << " val " << (int)rick.get_state(i) <<
-  //   std::endl;
-  // }
+  // Poll forever, updating printout on new data
+  while (1) {
+    printTable(curGPIO, curRPM, curAccel, curBrake);
 
-  // sleep(2);
+    busMonitor.canRead(&newMessage);
 
-  // for (int i = 0; i < MCP_NUM_PINS; i++) {
-  //   rick.set_state(i, 0);
-  //   usleep(1000);
-  //   std::cout << "Pin " << +i << " val " << (int)rick.get_state(i) <<
-  //   std::endl;
-  // }
-
-  // std::cout << "Morty\n";
-  // for (int i = 0; i < MCP_NUM_PINS; i++) {
-  //   morty.set_state(i, 1);
-  //   usleep(1000);
-  //   std::cout << "Pin " << +i << " val " << (int)morty.get_state(i)
-  //             << std::endl;
-  // }
-
-  // sleep(2);
-
-  // for (int i = 0; i < MCP_NUM_PINS; i++) {
-  //   morty.set_state(i, 0);
-  //   usleep(1000);
-  //   std::cout << "Pin " << +i << " val " << (int)morty.get_state(i)
-  //             << std::endl;
-  // }
-
-  Ina3221 test = Ina3221(2, 0x40, 0.005, 0.005, 0.005);
-  test.begin();
-  std::cout << "Ina3221" << std::endl;
-  for (int i = 0; i < 1000; i++) {
-    // std::cout << "channel 1 voltage: " << test.readVoltage(0x1) << std::endl;
-    // std::cout << "channel 2 voltage: " << test.readVoltage(0x2) << std::endl;
-    // std::cout << "channel 3 voltage: " << test.readVoltage(0x3) << std::endl;
-    std::cout << "channel 3 current: " << test.readCurrent(0x3) << std::endl;
-    std::cout << std::endl;
-    usleep(1000000);
+    // Check if from MCC
+    if (newMessage.can_id & 0x200) {
+      switch (newMessage.can_id & 0x7) {
+        case 0:
+          curRPM = *(float*)newMessage.data;
+          break;
+        case 1:
+          curGPIO = *(uint16_t*)newMessage.data;
+          break;
+        case 2:
+          curAccel = *(float*)newMessage.data;
+        case 3:
+          curBrake = *(float*)newMessage.data;
+          break;
+        default:
+          break;
+      }
+    }
   }
 
   return 0;

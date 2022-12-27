81b80f0ff6789668d37a60a301322bb4252d48e8
Marco Kurzynski <marcokurzynski@icloud.com>
2022-12-27 02:41:51 +0000

    better test

--- btf/src/can_test.cpp
+++ btf/src/can_test.cpp
@@ -11,33 +11,27 @@ static Can r("vcan0");
 
 TEST(Can, callbacks) {
   volatile uint32_t flag = 0;
-
-  EXPECT_EQ(s.init(), 0);
-  EXPECT_EQ(r.init(), 0);
-
   vector<callback> callbacks{
       {0x21,
        [&flag](struct can_frame &frame) { flag = *(uint32_t *)frame.data; }},
       {0x39, [&flag](struct can_frame &frame) { flag = 0x1234; }},
   };
+
   CanDevice cd(r, callbacks);
 
-  int pid = fork();
-  if (pid == 0) {
-    uint32_t data = 0xDEADBEEF;
-    s.send(0x21, (uint8_t *)&data, sizeof(data));
+  EXPECT_EQ(s.init(), 0);
+  EXPECT_EQ(r.init(), 0);
 
-    while (flag == data)
-      ;
-    data = 0xBAD637;
-    s.send(0x39, (uint8_t *)&data, sizeof(data));
-    exit(0);
-  }
+  uint32_t data = 0xDEADBEEF;
+  s.send(0x21, (uint8_t *)&data, sizeof(data));
 
   while (flag == 0)
     ;
   EXPECT_EQ(flag, 0xDEADBEEF);
   flag = 0;
+
+  data = 0xBAD637;
+  s.send(0x39, (uint8_t *)&data, sizeof(data));
   while (flag == 0)
     ;
   EXPECT_EQ(flag, 0x1234);

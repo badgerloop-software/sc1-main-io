00f7958c4e1ede3c359470cc26969c663a32b9de
Marco Kurzynski <marcokurzynski@icloud.com>
2023-01-12 02:12:11 +0000

    tests

--- toolchains/beaglebone_aarch64.cmake
+++ toolchains/beaglebone_aarch64.cmake
@@ -3,8 +3,8 @@ SET(CMAKE_SYSTEM_NAME BeagleBone)
 
 # Specify the cross compiler
 
-SET(CMAKE_C_COMPILER   /home/mkurzynski/buildroot/arm-buildroot-linux-gnueabihf_sdk-buildroot/bin/arm-buildroot-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER /home/mkurzynski/buildroot/arm-buildroot-linux-gnueabihf_sdk-buildroot/bin/arm-buildroot-linux-gnueabihf-g++)
+SET(CMAKE_C_COMPILER   /opt/arm-buildroot-linux-gnueabihf_sdk-buildroot/bin/arm-buildroot-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /opt/arm-buildroot-linux-gnueabihf_sdk-buildroot/bin/arm-buildroot-linux-gnueabihf-g++)
 
 # Search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)

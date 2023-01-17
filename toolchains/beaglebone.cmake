f99fc080c3220e6aebe3f51f5b7b8b321c1f8edc
Marco Kurzynski <marcokurzynski@icloud.com>
2023-01-17 07:48:32 +0000

    got buildroot usb ethernet

--- toolchains/beaglebone.cmake
+++ toolchains/beaglebone.cmake
@@ -2,8 +2,8 @@
 SET(CMAKE_SYSTEM_NAME BeagleBone)
 
 # Specify the cross compiler
-SET(CMAKE_C_COMPILER   /opt/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER /opt/gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-g++)
+SET(CMAKE_C_COMPILER   /opt/arm-buildroot-linux-gnueabihf_sdk-buildroot/bin/arm-buildroot-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /opt/arm-buildroot-linux-gnueabihf_sdk-buildroot/bin/arm-buildroot-linux-gnueabihf-g++)
 
 # Search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
